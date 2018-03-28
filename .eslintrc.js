67703a65022a6354837c0e832bffaafe6f757418
许广宇 <xugy@192.168.1.4>
2018-03-28 23:44:23 +0800

    eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,56 @@
 module.exports = {
-	env: {
-		node: true,
+	// 指定环境
+	"env": {
+		"browser": true,
+		"node": true,
+		"commonjs": true,
+		"worker": true,
+		"es6": true,
+		"jest": true,
+		"jasmine": true,
+		"phantomjs": true,
+		"protractor": true,
+		"qunit": true,
+		"shelljs": true,
+		"serviceworker": true,
+		"webextensions": true,
+		"greasemonkey": true,
+		"mongo": true
 	},
+	// 开启参数
+	"parserOptions": {
+		// "ecmaVersion": 6,
+		"globalReturn": true,
+        "sourceType": "module",
+    	"ecmaFeatures": {
+            "jsx": true
+        }
+	},
+	// 全局变量
+	"globals": {
+        "var1": true, // 可写
+        "var2": false // 只读
+    },
+	// 使用规则
 	// "extends": "airbnb-base"
-	extends: "eslint:recommended",
-	// plugins: ["prettier"],
-	rules: {
-		indent: ["error", "tab"],
+	"extends": "eslint:recommended",
+	// 使用组件在特定环境下
+	// plugins: ["prettier"], // 全局安装的 ESLint 实例只能使用全局安装的 ESLint 插件，本地安装的版本，只能用 本地安装 的插件。不支持混合本地和全局插件。
+	// "env": {
+	// 	"prettier/browser": true
+	// },
+	// 设置规则
+	"rules": {
+		"no-global-assign": ["error", {"exceptions": [{
+			"var1": true
+		}]}],
+		"no-redeclare": ["error", { "builtinGlobals": true }],
+		"no-shadow": ["error", { "builtinGlobals": false, "hoist": "functions", "allow": [] }],
+		"indent": ["error", "tab"],
 		"linebreak-style": ["error", "unix"],
-		quotes: ["error", "single"],
-		semi: ["error", "always"],
-		// "prettier/prettier": [
+		"quotes": ["off", "single"],
+		"semi": ["warn", "always"],
+		// "prettier/prettier": [ // 插件名/规则ID
 		//     "error",
 		//     {
 		//         "printWidth": 100,
